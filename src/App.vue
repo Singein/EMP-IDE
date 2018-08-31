032ba5289414f003f4e2e490f4d0afc7904cef5b
How <Fuermohao@outlook.com>
2018-08-31 16:14:19 +0800

    added: file download

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,7 @@
                         </mu-button>
                     
              
-                        <mu-button v-show="i.index===list_index&&!i.children" icon small @click="update_code">
+                        <mu-button v-show="i.index===list_index&&!i.children" icon small @click="get_file">
                           <mu-icon value="get_app" color="blue"></mu-icon>
                         </mu-button>
                      
@@ -89,7 +89,7 @@
                         </mu-button>
                      
                   
-                        <mu-button v-show="j.index===list_index" icon small @click="update_code">
+                        <mu-button v-show="j.index===list_index" icon small @click="get_file">
                           <mu-icon value="get_app" color="blue"></mu-icon>
                         </mu-button>
                    
@@ -327,6 +327,8 @@
 import Terminal from "../term.js";
 var put_file_data = null;
 var put_file_name = null;
+var get_file_name = null;
+var get_file_data = null;
 export default {
   name: "App",
   components: {},
@@ -366,10 +368,7 @@ export default {
       // websocket 返回数据接受对象
       ws_return: "",
 
-      button_text: "connect",
-
-      get_file_name: null,
-      get_file_data: null
+      button_text: "connect"
     };
   },
 
@@ -450,7 +449,8 @@ export default {
       if (!is_dir) {
         if (
           this.last_command === "excute_script" ||
-          this.last_command.startsWith("del")
+          this.last_command.startsWith("del") ||
+          this.last_command === "download_file"
         ) {
           // do nothing
         } else {
@@ -559,44 +559,6 @@ def del_file(filename):
       this.put_file();
     },
 
-    put_file() {
-      var dest_fname = put_file_name;
-      var dest_fsize = put_file_data.length;
-
-      // WEBREPL_FILE = "<2sBBQLH64s"
-      var rec = new Uint8Array(2 + 1 + 1 + 8 + 4 + 2 + 64);
-      rec[0] = "W".charCodeAt(0);
-      rec[1] = "A".charCodeAt(0);
-      rec[2] = 1; // put
-      rec[3] = 0;
-      rec[4] = 0;
-      rec[5] = 0;
-      rec[6] = 0;
-      rec[7] = 0;
-      rec[8] = 0;
-      rec[9] = 0;
-      rec[10] = 0;
-      rec[11] = 0;
-      rec[12] = dest_fsize & 0xff;
-      rec[13] = (dest_fsize >> 8) & 0xff;
-      rec[14] = (dest_fsize >> 16) & 0xff;
-      rec[15] = (dest_fsize >> 24) & 0xff;
-      rec[16] = dest_fname.length & 0xff;
-      rec[17] = (dest_fname.length >> 8) & 0xff;
-      for (var i = 0; i < 64; ++i) {
-        if (i < dest_fname.length) {
-          rec[18 + i] = dest_fname.charCodeAt(i);
-        } else {
-          rec[18 + i] = 0;
-        }
-      }
-
-      // initiate put
-      this.binary_state = 11;
-      this.update_file_status("Sending " + put_file_name + "...");
-      this.ws.send(rec);
-    },
-
     prepare_for_connect() {
       this.is_connected = false;
       this.button_text = "Connect";
@@ -673,6 +635,72 @@ def del_file(filename):
                 }
                 this.binary_state = 0;
                 break;
+              case 21:
+                // first response for get
+                if (this.decode_resp(data) == 0) {
+                  this.binary_state = 22;
+                  var rec = new Uint8Array(1);
+                  rec[0] = 0;
+                  this.ws.send(rec);
+                }
+                break;
+              case 22: {
+                // file data
+                var sz = data[0] | (data[1] << 8);
+                if (data.length == 2 + sz) {
+                  // we assume that the data comes in single chunks
+                  if (sz == 0) {
+                    // end of file
+                    this.binary_state = 23;
+                  } else {
+                    // accumulate incoming data to get_file_data
+                    var new_buf = new Uint8Array(get_file_data.length + sz);
+                    new_buf.set(get_file_data);
+                    new_buf.set(data.slice(2), get_file_data.length);
+                    get_file_data = new_buf;
+                    this.update_file_status(
+                      "Getting " +
+                        get_file_name +
+                        ", " +
+                        get_file_data.length +
+                        " bytes"
+                    );
+
+                    var rec = new Uint8Array(1);
+                    rec[0] = 0;
+                    this.ws.send(rec);
+                  }
+                } else {
+                  this.binary_state = 0;
+                }
+                break;
+              }
+              case 23:
+                // final response
+                if (this.decode_resp(data) == 0) {
+                  this.update_file_status(
+                    "Got " +
+                      get_file_name +
+                      ", " +
+                      get_file_data.length +
+                      " bytes"
+                  );
+                  saveAs(
+                    new Blob([get_file_data], {
+                      type: "application/octet-stream"
+                    }),
+                    get_file_name
+                  );
+                } else {
+                  this.update_file_status("Failed getting " + get_file_name);
+                }
+                this.binary_state = 0;
+                break;
+              case 31:
+                // first (and last) response for GET_VER
+                console.log("GET_VER", data);
+                this.binary_state = 0;
+                break;
             }
           }
           this.term.write(event.data);
@@ -711,6 +739,85 @@ def del_file(filename):
         document.getElementById("filename").innerHTML = put_file_name;
       };
       reader.readAsArrayBuffer(f);
+    },
+
+    get_file() {
+      this.last_command = "download_file";
+      // var src_fname = document.getElementById("get_filename").value;
+      var src_fname = this.opened_file;
+
+      // WEBREPL_FILE = "<2sBBQLH64s"
+      var rec = new Uint8Array(2 + 1 + 1 + 8 + 4 + 2 + 64);
+      rec[0] = "W".charCodeAt(0);
+      rec[1] = "A".charCodeAt(0);
+      rec[2] = 2; // get
+      rec[3] = 0;
+      rec[4] = 0;
+      rec[5] = 0;
+      rec[6] = 0;
+      rec[7] = 0;
+      rec[8] = 0;
+      rec[9] = 0;
+      rec[10] = 0;
+      rec[11] = 0;
+      rec[12] = 0;
+      rec[13] = 0;
+      rec[14] = 0;
+      rec[15] = 0;
+      rec[16] = src_fname.length & 0xff;
+      rec[17] = (src_fname.length >> 8) & 0xff;
+      for (var i = 0; i < 64; ++i) {
+        if (i < src_fname.length) {
+          rec[18 + i] = src_fname.charCodeAt(i);
+        } else {
+          rec[18 + i] = 0;
+        }
+      }
+
+      // initiate get
+      this.binary_state = 21;
+      get_file_name = src_fname;
+      get_file_data = new Uint8Array(0);
+      this.update_file_status("Getting " + get_file_name + "...");
+      this.ws.send(rec);
+    },
+
+    put_file() {
+      var dest_fname = put_file_name;
+      var dest_fsize = put_file_data.length;
+
+      // WEBREPL_FILE = "<2sBBQLH64s"
+      var rec = new Uint8Array(2 + 1 + 1 + 8 + 4 + 2 + 64);
+      rec[0] = "W".charCodeAt(0);
+      rec[1] = "A".charCodeAt(0);
+      rec[2] = 1; // put
+      rec[3] = 0;
+      rec[4] = 0;
+      rec[5] = 0;
+      rec[6] = 0;
+      rec[7] = 0;
+      rec[8] = 0;
+      rec[9] = 0;
+      rec[10] = 0;
+      rec[11] = 0;
+      rec[12] = dest_fsize & 0xff;
+      rec[13] = (dest_fsize >> 8) & 0xff;
+      rec[14] = (dest_fsize >> 16) & 0xff;
+      rec[15] = (dest_fsize >> 24) & 0xff;
+      rec[16] = dest_fname.length & 0xff;
+      rec[17] = (dest_fname.length >> 8) & 0xff;
+      for (var i = 0; i < 64; ++i) {
+        if (i < dest_fname.length) {
+          rec[18 + i] = dest_fname.charCodeAt(i);
+        } else {
+          rec[18 + i] = 0;
+        }
+      }
+
+      // initiate put
+      this.binary_state = 11;
+      this.update_file_status("Sending " + put_file_name + "...");
+      this.ws.send(rec);
     },
 
     decode_resp(data) {
