dba84a5937ff6d3a14883e51300e76b56b269624
How <Fuermohao@outlook.com>
2018-09-06 12:46:13 +0800

    added: uwsgi.ini

--- server/MicroIDE/MicroIDE/settings.py
+++ server/MicroIDE/MicroIDE/settings.py
@@ -106,16 +106,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectedstatic')
