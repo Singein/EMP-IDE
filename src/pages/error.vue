01831eeff8558c130fc18903f24cf9a3d5600ee4
鲍浙军 <razor.bao@dbappsecurity.com.cn>
2018-09-09 09:41:21 +0800

    added: add the error page

---
+++ src/pages/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    ERROR: {{errorCode}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  computed: {
+    errorCode () {
+      return this.$route.meta.errorCode || '404'
+    }
+  }
+}
+</script>
