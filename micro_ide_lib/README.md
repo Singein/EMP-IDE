c64267c44f668c51f5d3eaaa2f256e0a985ad055
How <Fuermohao@outlook.com>
2018-09-10 11:18:19 +0800

    added: add micro_ide_lib folder to  manage the microide module for micropython

---
+++ micro_ide_lib/README.md
@@ -0,0 +1,2 @@
+# microide.py 
+> 部署在MicroPython设备中的脚本固件,封装了包含文件操作,脚本执行的代码
---
+++ micro_ide_lib/deploy.py
@@ -0,0 +1,13 @@
+"""
+用于将microide.py的脚本 注入到 src/pages/ide/microide.py.js
+"""
+
+src_path = './microide.py'
+des_path = '../src/pages/ide/microide.py.js'
+codes = ''
+js_codes = 'var microide_codes=`{0}`;\nexport {\n  microide_codes\n}'
+with open(src_path, 'r') as f:
+    codes = f.read()
+
+with open(des_path, 'w') as f:
+    f.write(js_codes.format(codes))
