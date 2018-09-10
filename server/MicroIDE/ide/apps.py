d4abf21b9240cacf154a62c50385fbd4c3622875
How <Fuermohao@outlook.com>
2018-09-10 10:54:22 +0800

    rm: server module

--- server/MicroIDE/ide/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IdeConfig(AppConfig):
-    name = 'ide'
--- server/MicroIDE/ide/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from django.urls import include
-from . import views
-
-urlpatterns = [path('', views.index)]
