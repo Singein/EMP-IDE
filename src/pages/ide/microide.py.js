4b775c344f40530c628c7b697947a264814f1745
How <Fuermohao@outlook.com>
2018-09-18 18:49:46 +0800

    fixed: \ escape

--- src/pages/ide/microide.py.js
+++ src/pages/ide/microide.py.js
@@ -69,11 +69,11 @@ def rainbow(output, color=None):
     """
     if color:
         if color == 'green':
-            return '\033[1;32m%s\033[0m' % output
+            return '\\033[1;32m%s\\033[0m' % output
         if color == 'red':
-            return '\033[1;31m%s\033[0m' % output
+            return '\\033[1;31m%s\\033[0m' % output
         if color == 'blue':
-            return '\033[1;34m%s\033[0m' % output
+            return '\\033[1;34m%s\\033[0m' % output
     else:
         return output
 
