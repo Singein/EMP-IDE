ffc0b7c4f560c24d56c4a3ea73f7519e7c091f34
How <Fuermohao@outlook.com>
2018-10-13 11:21:08 +0800

    added: sort method added to tree

--- src/pages/ide/components/FolderTree/slots.js
+++ src/pages/ide/components/FolderTree/slots.js
@@ -2,8 +2,20 @@ var slots = {
   methods: {
     //打开设置窗口,点击设置按钮时触发
     slotUpdateTree(kwargs) {
+      
+      kwargs.treeData[0].children.sort(function (s1, s2) {
+        let x1 = s1.name.toUpperCase();
+        let x2 = s2.name.toUpperCase();
+        if (x1 < x2) {
+          return -1;
+        }
+        if (x1 > x2) {
+          return 1;
+        }
+        return 0;
+      });
       this.data = kwargs.treeData;
-      setTimeout(() => this.$send(this.SIGNAL_CLEAR(this)),300);
+      setTimeout(() => this.$send(this.SIGNAL_CLEAR(this)), 300);
     }
 
   }
