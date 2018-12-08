0fbcf18cd05bda9db214f7c5266423d5c63b8e49
Ockham's Razor <hellojuicees@gmail.com>
2018-12-08 18:55:18 +0800

    chore: change the baseUrl

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,14 @@ const webpack = require('webpack')
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+  ? '/static/'
+  : '/',
   configureWebpack: {
     plugins: [
-      new MonacoWebpackPlugin(),
+      new MonacoWebpackPlugin({
+        output: 'js'
+      }),
       new webpack.ContextReplacementPlugin(/lazy-debug-legacy(\\|\/)src/, __dirname)
     ],
     externals: {
